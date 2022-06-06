58cde5a4a0e72807f42607e71ac86346d553143b
Jennifer Melot <jtmelot@gmail.com>
2022-06-06 11:44:54 -0400

    Add styling to show node type

--- supply-chain/src/components/dashboard.js
+++ supply-chain/src/components/dashboard.js
@@ -8,6 +8,13 @@ import Arrow, { DIRECTION, HEAD } from "react-arrows";
 
 import {graph, graphReverse, nodeToMeta} from "../../data/graph";
 
+const typeToColor = {
+  "input_resource": "rgba(122, 196, 165, 0.75)",
+  "process": "rgba(21, 175, 208, 0.75)",
+  "stage": "rgba(124, 51, 111, 0.75)",
+  "tool_resource": "rgba(241, 127, 76, 0.75)",
+  "ultimate_output": "rgba(229, 191, 33, 0.75)",
+};
 
 const Dashboard = () => {
   const finalNode = Object.keys(nodeToMeta).filter(k => nodeToMeta[k]["type"] === "ultimate_output")[0];
@@ -15,7 +22,13 @@ const Dashboard = () => {
   const mkLayer = (edges) => {
     return <div>
       {edges.map(edge => {
-        return <Paper id={edge} style={{width: "200px", margin: "20px", display: "inline-block"}}>{edge}</Paper>
+        return <Paper id={edge} style={{width: "200px", height: "60px", padding: "5px",
+          margin: "50px 20px", display: "inline-block", backgroundColor: typeToColor[nodeToMeta[edge]["type"]]}}>
+          <div>
+            {edge}
+            <Typography component={"p"} variant={"body2"}>{nodeToMeta[edge]["name"]}</Typography>
+          </div>
+        </Paper>
       })}
     </div>
   };
