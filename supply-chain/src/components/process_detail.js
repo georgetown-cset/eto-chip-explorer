38d3b1a1370b977bffceb7f65a87aa68500a0479
Jennifer Melot <jtmelot@gmail.com>
2022-07-02 22:14:38 -0400

    Begin integrating tool/material metadata into expanded process view

--- supply-chain/src/components/process_detail.js
+++ supply-chain/src/components/process_detail.js
@@ -9,35 +9,33 @@ import GraphNode from "./graph_node";
 const ProcessDetail = (props) => {
   const {selectedNode, descriptions} = props;
 
-  /*const mkNodeGraph = () => {
-    return <div>
-      <div style={{width: "33%", display: "inline-block", verticalAlign: "middle"}}>
-        {nodeToMeta[selectedNode]["materials"].map((node) =>
-          <GraphNode node={node} meta={nodeToMeta[node]}
-                   unattached={false} description={data.allMdx.nodes.filter(n => n.slug === node)[0].body}
-                   setSelected={setSelectedNode}/>
-        )}
-      </div>
-      <div style={{width: "33%", display: "inline-block", verticalAlign: "middle"}}>
-        <GraphNode node={selectedNode} meta={nodeToMeta[selectedNode]} expanded={true}
-                   unattached={false} description={data.allMdx.nodes.filter(n => n.slug === selectedNode)[0].body}
-                   setSelected={setSelectedNode} />
-      </div>
-      <div style={{width: "33%", display: "inline-block", verticalAlign: "middle"}}>
-        {nodeToMeta[selectedNode]["tools"].map((node) =>
-          <GraphNode node={node} meta={nodeToMeta[node]}
-                   unattached={false} description={data.allMdx.nodes.filter(n => n.slug === node)[0].body}
-                   setSelected={setSelectedNode}/>
-        )}
-      </div>
-    </div>
-  }*/
-
   return (
-    <div style={{width: "49%", display: "inline-block"}}>
+    <div style={{display: "inline-block", padding: "0px 40px"}}>
       <MDXProvider>
         <MDXRenderer>{descriptions.filter(n => n.slug === selectedNode)[0].body}</MDXRenderer>
       </MDXProvider>
+      {"materials" in nodeToMeta[selectedNode] && (nodeToMeta[selectedNode]["materials"].length > 0) &&
+      <div>
+        <Typography component={"p"} variant={"h6"}>Material Components</Typography>
+        {nodeToMeta[selectedNode]["materials"].map((node) =>
+          <div>
+            <MDXProvider>
+              <MDXRenderer>{descriptions.filter(n => n.slug === node)[0].body}</MDXRenderer>
+            </MDXProvider>
+          </div>
+        )}
+      </div>}
+      {"tools" in nodeToMeta[selectedNode] && (nodeToMeta[selectedNode]["tools"].length > 0) &&
+      <div>
+        <Typography component={"p"} variant={"h6"}>Tools</Typography>
+        {nodeToMeta[selectedNode]["tools"].map((node) =>
+          <div>
+            <MDXProvider>
+              <MDXRenderer>{descriptions.filter(n => n.slug === node)[0].body}</MDXRenderer>
+            </MDXProvider>
+          </div>
+        )}
+      </div>}
     </div>
   )
 };
