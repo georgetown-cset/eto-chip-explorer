f6318d52419fe4cb912437acb70338606a0b983d
Jennifer Melot <jtmelot@gmail.com>
2022-08-17 23:58:43 -0400

    Refactor preprocess into class

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -1,16 +1,16 @@
 import unittest
 
-from scripts.preprocess import get_country
+from scripts.preprocess import Preprocess
 
 class TestPreprocess(unittest.TestCase):
     def test_get_country_alpha3(self):
-        self.assertEqual("United States", get_country("USA"))
+        self.assertEqual("United States", Preprocess.get_country("USA"))
 
     def test_get_country_alpha3_mapped(self):
-        self.assertEqual("Taiwan", get_country("TWN"))
+        self.assertEqual("Taiwan", Preprocess.get_country("TWN"))
 
     def test_get_country_no_alpha3(self):
-        self.assertEqual("Europe", get_country("Europe"))
+        self.assertEqual("Europe", Preprocess.get_country("Europe"))
 
     def test_get_country_no_alpha3_mapped(self):
-        self.assertEqual("Malaysia", get_country("MAL"))
+        self.assertEqual("Malaysia", Preprocess.get_country("MAL"))
