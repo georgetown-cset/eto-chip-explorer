3a22f52a64c9c9a32d21026255b93d1496e6f5f1
Neha Singh <ns1375@georgetown.edu>
2022-08-30 14:42:01 -0400

    Implement dropdowns

--- supply-chain/src/components/dashboard.js
+++ supply-chain/src/components/dashboard.js
@@ -5,51 +5,44 @@ import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import OutlinedInput from "@mui/material/OutlinedInput";
-import Typography from "@mui/material/Typography";
 import Select from "@mui/material/Select";
 import Paper from "@mui/material/Paper";
-import { Header as ETOHeader, Footer } from "@eto/eto-ui-components";
+import { Header as ETOHeader, Footer, Dropdown } from "@eto/eto-ui-components";
 
 import Header from "./header";
 import Map from "./map";
 import { nodeToMeta } from "../../data/graph";
 import { countryProvision } from "../../data/provision";
 
-function getStyles(name, selectedName, theme) {
-  return {
-    fontWeight:
-      selectedName !== name
-        ? theme.typography.fontWeightRegular
-        : theme.typography.fontWeightMedium,
-  };
-}
-
 const Dashboard = () => {
 
-  const getMaterialToNodes = () => {
-    const materialToNode = {};
+  const getInputToNodes = () => {
+    const inputTypes = ["materials", "tools"];
+    const inputToNode = {};
     for(let node in nodeToMeta){
       if(nodeToMeta[node]["type"] === "process"){
-        for(let material of nodeToMeta[node]["materials"]){
-          if(!(material in materialToNode)){
-            materialToNode[material] = {};
+        for (let inputType of inputTypes) {
+          for(let input of nodeToMeta[node][inputType]){
+            if(!(input in inputToNode)){
+              inputToNode[input] = {};
+            }
+            inputToNode[input][node] = 1;
           }
-          materialToNode[material][node] = 1;
         }
       }
     }
-    return materialToNode;
+    return inputToNode;
   };
 
   const getCurrentHighlights = (currFilterValues = filterValues) => {
-    let highlighter = "material-resource";
+    let highlighter = "input-resource";
     for(let fv in defaultFilterValues){
       if(defaultFilterValues[fv] !== currFilterValues[fv]){
         highlighter = fv;
       }
     }
     const currMapping = filterToValues[highlighter];
-    if(highlighter === "material-resource") {
+    if(highlighter === "input-resource") {
       const identityMap = {}
       identityMap[currFilterValues[highlighter]] = 1
       setHighlights(identityMap)
@@ -58,24 +51,24 @@ const Dashboard = () => {
     }
   };
 
-  const materialToNode = getMaterialToNodes();
+  const inputToNode = getInputToNodes();
   const theme = useTheme();
-  const filterKeys = ["material-resource", "country"];
+  const filterKeys = ["input-resource", "country"];
   const defaultFilterValues = {
-    "material-resource": "All",
-    "country": "All",
+    "input-resource": "All",
+    "country": ["All"],
   };
   const filterToValues = {
-    "material-resource": materialToNode,
+    "input-resource": inputToNode,
     "country": countryProvision
   };
   const [filterValues, setFilterValues] = React.useState(defaultFilterValues);
   const [highlights, setHighlights] = React.useState({});
 
-  const handleChange = (evt, key) => {
+  const handleChange = (val, key) => {
     const updatedFilterValues = {...defaultFilterValues};
     if (key !== null) {
-      updatedFilterValues[key] = evt.target.value;
+      updatedFilterValues[key] = val;
     }
     setFilterValues(updatedFilterValues);
     getCurrentHighlights(updatedFilterValues);
@@ -94,86 +87,69 @@ const Dashboard = () => {
     window.history.replaceState(null, null, window.location.pathname + "?" + urlParams.toString());
   };
 
+  // Functions to interface with ETO dropdown component
+  const handleCountryChange = (val) => {
+    handleChange(val, "country");
+  };
+  const countryOptions = [{"val": "All", "text": "All"}];
+  Object.keys(countryProvision).sort().filter((c) => c !== "Other").map((name) => (
+    countryOptions.push({"val": name, "text": name})
+  ));
+
+  const handleInputResourceChange = (val) => {
+    handleChange(val, "input-resource");
+  };
+  const inputResourceOptions = [{"val": "All", "text": "All"}];
+  Object.keys(inputToNode).sort(
+    (a, b) => ('' + nodeToMeta[a]["name"]).localeCompare(nodeToMeta[b]["name"])
+  ).map((name) => (
+    inputResourceOptions.push({"val": name, "text": nodeToMeta[name]["name"]})
+  ));
+
   // Sets the state of the app based on the queries in the URL.
   // This will only run once, when the component is initially rendered.
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const updatedFilterValues = {...defaultFilterValues};
     for (const filterKey of filterKeys) {
-      const filterVal = urlParams.get(filterKey);
+      let filterVal = urlParams.get(filterKey);
       if (filterVal) {
+        if (filterKey === "country") {
+          // This is a multi-select, so we need to pass in an array
+          filterVal = filterVal.split(",");
+        }
         updatedFilterValues[filterKey] = filterVal;
       }
     }
     setFilterValues(updatedFilterValues);
     getCurrentHighlights(updatedFilterValues);
-  }, [])
+  }, []);
 
   return (<div>
     <ETOHeader/>
     <Header/>
     <Paper style={{paddingBottom: "20px", marginBottom: "5px", position: "sticky", top: "0px", width: "100%", zIndex: "10"}}
       className="filter-bar"
+      elevation={0}
     >
       <div style={{display: "inline-block"}}>
-      <FormControl sx={{m: 1}} size={"small"} style={{margin: "15px 0 0 15px", textAlign: "left", minWidth: "200px"}}>
-        <InputLabel id="country-select-label">Countries</InputLabel>
-        <Select
-          labelId="country-select-label"
-          id="country-select"
-          value={filterValues["country"]}
-          onChange={e => handleChange(e, "country")}
-          input={<OutlinedInput label={"Country provision share"}/>}
-        >
-          <MenuItem
-            key={"All"}
-            value={"All"}
-            style={getStyles("All", filterValues["country"], theme)}
-            >
-              All
-          </MenuItem>
-          {Object.keys(countryProvision).sort().filter((c) => c !== "Other").map((name) => (
-            <MenuItem
-              key={name}
-              value={name}
-              style={getStyles(name, filterValues["country"], theme)}
-            >
-              {name}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+        <Dropdown
+          inputLabel="Countries"
+          selected={filterValues["country"]}
+          setSelected={handleCountryChange}
+          multiple="true"
+          options={countryOptions}
+        />
       </div>
       <div style={{display: "inline-block"}}>
-      <FormControl sx={{m: 1}} size={"small"} style={{margin: "15px 0 0 15px", textAlign: "left", minWidth: "200px"}}>
-        <InputLabel id="material-select-label">Inputs</InputLabel>
-        <Select
-          labelId="material-select-label"
-          id="material-select"
-          value={filterValues["material-resource"]}
-          onChange={e => handleChange(e, "material-resource")}
-          input={<OutlinedInput label={"Material component"}/>}
-        >
-          <MenuItem
-            key={"All"}
-            value={"All"}
-            style={getStyles("All", filterValues["material-resource"], theme)}
-            >
-              All
-          </MenuItem>
-          {Object.keys(materialToNode).sort((a, b) => nodeToMeta[a]["name"] > nodeToMeta[b]["name"]).map((name) => (
-            <MenuItem
-              key={name}
-              value={name}
-              style={getStyles(name, filterValues["material-resource"], theme)}
-            >
-              {nodeToMeta[name]["name"]}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+        <Dropdown
+          inputLabel="Inputs"
+          selected={filterValues["input-resource"]}
+          setSelected={handleInputResourceChange}
+          options={inputResourceOptions}
+        />
       </div>
-      <Button style={{float: "right", marginRight: "10px"}} onClick={(evt) => handleChange(evt, null)}>
+      <Button id="clear-button" style={{float: "right", marginRight: "10px"}} onClick={(evt) => handleChange(evt, null)}>
         Clear
       </Button>
     </Paper>
