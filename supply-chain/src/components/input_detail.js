af327b71e30055cc3b19c2c576cb044d24b6f1f0
Neha Singh <ns1375@georgetown.edu>
2022-09-12 15:13:00 -0400

    Closes #244

--- supply-chain/src/components/input_detail.js
+++ supply-chain/src/components/input_detail.js
@@ -97,7 +97,7 @@ const InputDetail = (props) => {
             <Typography component="p">
               {orgMeta[org]["hq"] && <span className="flag">{orgMeta[org]["hq"]}</span>}
               {orgMeta[org]["name"]}
-              {orgs[org] !== "Major" && <span> ({orgs[org]} provider)</span>}
+              {orgs[org] !== "Major" && <span> ({orgs[org]} market share)</span>}
             </Typography>
           </td>
           ))}
