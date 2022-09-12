053952c7f2cbc905b3f0141401f0d5300063ef79
Jennifer Melot <jtmelot@gmail.com>
2022-09-12 10:27:17 -0400

    Update appintro text integration

--- supply-chain/src/components/dashboard.js
+++ supply-chain/src/components/dashboard.js
@@ -356,14 +356,16 @@ const Dashboard = () => {
 
   return (<AppWrapper>
     <div style={{margin: "40px 20px 40px 100px", maxWidth: "1200px"}}>
-    <AppIntro title={"Supply Chain Explorer"} content={<Typography component={"div"} variant={"body1"}>
-      ETO’s Supply Chain Explorer visualizes supply chains in critical and emerging technology.
-      This edition of the Explorer covers the essential tools, materials, processes, countries,
-      and firms involved in producing advanced logic chips. It’s built to help users who are not
-      semiconductor experts get up to speed on how this essential technology is produced, and
-      to allow users of all backgrounds to visually explore how different inputs, companies,
-      and nations interact in the production process.
-    </Typography>}/>
+    <AppIntro title={"Supply Chain Explorer"}>
+      <Typography component={"div"} variant={"body1"}>
+        ETO’s Supply Chain Explorer visualizes supply chains in critical and emerging technology.
+        This edition of the Explorer covers the essential tools, materials, processes, countries,
+        and firms involved in producing advanced logic chips. It’s built to help users who are not
+        semiconductor experts get up to speed on how this essential technology is produced, and
+        to allow users of all backgrounds to visually explore how different inputs, companies,
+        and nations interact in the production process.
+      </Typography>
+    </AppIntro>
     </div>
     <Paper style={{position: "sticky", top: "0px", zIndex: "10"}}
       className="filter-bar"
