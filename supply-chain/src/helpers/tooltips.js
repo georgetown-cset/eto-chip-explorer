4c4742d37cd36f4fcd50bb3b5ed9745104d971cd
Jennifer Melot <jtmelot@gmail.com>
2022-09-12 10:34:39 -0400

    ...actually add the tooltips template file

---
+++ supply-chain/src/helpers/tooltips.js
@@ -0,0 +1,10 @@
+import {FILTER_INPUT, FILTER_COUNTRY, FILTER_CONCENTRATION, FILTER_ORG} from "../helpers/shared";
+
+const tooltips = {
+  [FILTER_INPUT]: "...",
+  [FILTER_COUNTRY]: "...",
+  [FILTER_ORG]: "...",
+  [FILTER_CONCENTRATION]: "..."
+};
+
+export default tooltips;
