b6c398c5f72bd08c4527c8c702190664b8a35315
za158 <58824955+za158@users.noreply.github.com>
2022-09-12 15:55:40 -0400

    Add text

--- supply-chain/src/helpers/tooltips.js
+++ supply-chain/src/helpers/tooltips.js
@@ -1,10 +1,10 @@
 import {FILTER_INPUT, FILTER_COUNTRY, FILTER_CONCENTRATION, FILTER_ORG} from "../helpers/shared";
 
 const tooltips = {
-  [FILTER_INPUT]: "...",
-  [FILTER_COUNTRY]: "...",
-  [FILTER_ORG]: "...",
-  [FILTER_CONCENTRATION]: "..."
+  [FILTER_INPUT]: "Use this option to jump to a specified stage, process, tool, or material in the supply chain.",
+  [FILTER_COUNTRY]: "Highlights chip inputs supplied by a specified country or set of countries.",
+  [FILTER_ORG]: "Highlights chip inputs supplied by a specified company or set of companies.",
+  [FILTER_CONCENTRATION]: "Highlights chip inputs supplied by a small number of countries."
 };
 
 export default tooltips;
