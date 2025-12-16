35fb23d627914ea7f57d32b1072cf85417623eb9
Brian Love <bl711@georgetown.edu>
2025-12-16 16:33:58 -0500

    Update deploy script to tag current/previous release
    Use tags instead of branches to tag the current and previous version

--- push_to_production.sh
+++ push_to_production.sh
@@ -9,4 +9,6 @@ gatsby clean && \
 gatsby build && \
 gsutil -m rsync -r -d public "gs://eto-tmp/eto-supply-chain/public" && \
 gsutil -m -h "Cache-Control:no-cache, max-age=0" rsync -r -d "gs://eto-tmp/eto-supply-chain/public" "gs://chipexplorer.eto.tech/" && \
-git branch --force deployed/production HEAD
+git tag --force deploy/previous deploy/current && \
+git tag --force deploy/current HEAD && \
+git push --force origin deploy/previous deploy/current
